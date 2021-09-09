b818b33470c5384317303524788e81d0e60b95e9
Dani-Iliev <75176422+Dani-Iliev@users.noreply.github.com>
2021-09-09 20:45:30 +0300

    Add Class Page in Models

--- CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShoppingCart.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CmsShoppingCart.Infrastructure
@@ -13,5 +14,6 @@ namespace CmsShoppingCart.Infrastructure
         {
             
         }
+        public DbSet<Page> Pages { get; set;}
     }
 }
